f1eb3a69ff83906b67ba3b0864cf1b7df79fae15
Mike Sinnema <msinnema33@gmail.com>
2020-02-24 18:05:43 -0700

    all tests working

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,70 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, wait } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
 import App from './App';
+// import { fetchshow as mockFetchShow } from './api/fetchshow';
+
+
+
+// const showData = {
+//     id: 2993,
+//     name: "Stranger Things",
+//     runtime: 60,
+//     image: {
+//       medium:
+//         "http://static.tvmaze.com/uploads/images/medium_portrait/200/501942.jpg",
+//       original:
+//         "http://static.tvmaze.com/uploads/images/original_untouched/200/501942.jpg"
+//     },
+//     summary:
+//       "<p>A love letter to the '80s classics that captivated a generation, <b>Stranger Things</b> is set in 1983 Indiana, where a young boy vanishes into thin air. As friends, family and local police search for answers, they are drawn into an extraordinary mystery involving top-secret government experiments, terrifying supernatural forces and one very strange little girl.</p>",
+//     _embedded: {
+//       episodes: [
+//         {
+//           id: 553946,
+//           name: "Chapter One: The Vanishing of Will Byers",
+//           season: 1,
+//           number: 1,
+//           runtime: 60,
+//           image: {
+//             medium:
+//               "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+//             original:
+//               "http://static.tvmaze.com/uploads/images/original_untouched/67/168918.jpg"
+//           }
+//         },
+//         {
+//           id: 909340,
+//           name: "Chapter One: MADMAX",
+//           season: 2,
+//           number: 1,
+//           runtime: 60,
+//           image: {
+//             medium:
+//               "http://static.tvmaze.com/uploads/images/medium_landscape/132/331976.jpg",
+//             original:
+//               "http://static.tvmaze.com/uploads/images/original_untouched/132/331976.jpg"
+//           }
+//         },
+//         {
+//           id: 1576469,
+//           name: "Chapter One: Suzie, Do You Copy?",
+//           season: 3,
+//           number: 1,
+//           runtime: 51,
+//           image: {
+//             medium:
+//               "http://static.tvmaze.com/uploads/images/medium_landscape/204/510098.jpg",
+//             original:
+//               "http://static.tvmaze.com/uploads/images/original_untouched/204/510098.jpg"
+//           }
+//         }
+//       ]
+//     }
+//   };
+
+
+jest.mock('./api/fetchshow.js');
 
 test("renders App without crashing", () => {
     render(<App />);
@@ -9,13 +72,17 @@ test("renders App without crashing", () => {
 
 
 
-test('User is able to select a season', () => {
+test('User is able to select a season', async() => {
+// mockFetchShow.mockResolvedValueOnce(showData);
+const { getByText,getAllByText } = render(<App />);
 
-const { getByText } = render(<App />);
+await wait(() =>{
+    getByText(/select a season/i)
+}) 
 
 
 userEvent.click(getByText(/select a season/i))
-getByText(/season 1/i);
+expect(getAllByText(/season /i)).toHaveLength(4);
 
 });
 
